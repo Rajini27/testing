d212f5c317886e12de94e66a557f08c9a849d358
Rajini27 <rajinip0808@gmail.com>
2024-07-10 13:49:44 +0100

    Update LoginRunner.java

--- src/test/java/Runner/LoginRunner.java
+++ src/test/java/Runner/LoginRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
+@RunWith(Cucumber.classs)
 @CucumberOptions(features={"src/test/resources/feature/postSteps.feature"},glue={"StepsDefination"},dryRun= false)
 
 public class LoginRunner {
