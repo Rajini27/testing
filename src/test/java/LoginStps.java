62bbe68a8bc11c2acb1f9e1fd6e355fc5040bc8b
Rajini27 <rajinip0808@gmail.com>
2024-07-10 12:10:16 +0100

    this is my compare branch commit

--- src/test/java/LoginStps.java
+++ src/test/java/LoginStps.java
@@ -1,5 +1,4 @@
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
-import io.cucumber.java.en.And;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
